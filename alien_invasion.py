ac58b57867ce6633078c6f3d9752413df01c7d34
Kerokeropyu <sharkxhu7@gmail.com>
2023-09-07 23:48:24 +0900

    宇宙船の表示、左右移動

---
+++ alien_invasion.py
@@ -0,0 +1,48 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvation:
+    """ゲームのアセットと動作を管理する全体的なクラス"""
+
+    def __init__(self) -> None:
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("エイリアン侵略")
+        self.ship = Ship(self)
+    
+    def run_game(self):
+        """ゲームのメインループを開始する"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        """キーボードとマウスのイベントに対応する"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        """画面上の画面を更新し、新しい画面に切り替える"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+if __name__ == '__main__':
+    # ゲームのインスタンスを作成し、ゲームを実行する
+    ai = AlienInvation()
+    ai.run_game()
---
+++ ship.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Ship:
+    """宇宙船を管理するクラス"""
+    def __init__(self, ai_game) -> None:
+        """宇宙船を初期化し、開始時の位置を設定する"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        """宇宙船を読み込み、サイズを取得する"""
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        """新しい宇宙船を画面下部の中央に配置する"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        # 移動フラグ
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitme(self):
+        """宇宙船を現在位置に描画する"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """移動フラグによって宇宙船の位置を更新する"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1

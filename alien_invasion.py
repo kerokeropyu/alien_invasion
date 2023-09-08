8ec265c0e6c7c53059e8a140052d95ba44afe576
Kerokeropyu <sharkxhu7@gmail.com>
2023-09-08 23:55:29 +0900

    弾を撃てるようにする、一度に撃てる弾の上限を設定

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,23 +2,29 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvation:
     """ゲームのアセットと動作を管理する全体的なクラス"""
 
     def __init__(self) -> None:
+        """ゲームを初期化し、ゲームのリソースを作成する"""
         pygame.init()
         self.settings = Settings()
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("エイリアン侵略")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def run_game(self):
         """ゲームのメインループを開始する"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -39,6 +45,8 @@ class AlienInvation:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullets()
     
     def _check_keyup_events(self, event):
         """キーをイベントに対応する"""
@@ -46,11 +54,29 @@ class AlienInvation:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    
+    def _fire_bullets(self):
+        """新しい弾を生成し、bulletsグループに返却する"""
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """弾の位置を更新し、古い弾を廃棄する"""
+        # 弾の位置を更新する
+        self.bullets.update()
+
+        # 見えなくなった弾を廃棄する
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """画面上の画面を更新し、新しい画面に切り替える"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 if __name__ == '__main__':
